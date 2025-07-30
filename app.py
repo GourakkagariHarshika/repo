80b3e8e067658e74009d5493203c86b2dc7ff3aa
GourakkagariHarshika <harshikasagar2713@gmail.com>
2025-07-30 15:09:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask Import "flask" could not be resolved
+import os
+app=Flask(__name__)
+@app.route("/")
+def hello():
+    return "hello world"
+if __name__=="__main__":
+    port=int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port)
